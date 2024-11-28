6d05d59fff9f99ea8542f4fdc94dd8f7e32c7c14
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-29 01:55:27 +0500

    uncomment code

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -16,50 +16,50 @@ function Quiz() {
                 'To manage state in a React functional component.', 'o update the DOM directly.'],
             answer: 'To manage state in a React functional component.',
         },
-        // {
-        //     question: "Q3) What is JSX in React",
-        //     option: ['A JavaScript library for DOM manipulation.', 'A syntax extension for JavaScript that looks like XML or HTML.',
-        //         'A React component lifecycle method.', 'A function to update state in React.'],
-        //     answer: 'A syntax extension for JavaScript that looks like XML or HTML.',
-        // },
-        // {
-        //     question: "Q4) Which React hook is used to handle side effects in a functional component",
-        //     option: ['useState', 'useEffect', 'useRef', 'useReducer'],
-        //     answer: 'useEffect',
-        // },
-        // {
-        //     question: "Q5) What is the default method for creating a React component",
-        //     option: ['Function declaration', 'Arrow function', 'Class-based approach', 'All of the above'],
-        //     answer: 'All of the above',
-        // },
-        // {
-        //     question: "Q6) What is the key prop used for in a React list",
-        //     option: ['To identify the type of data in a list.', 'To uniquely identify each element in a list.',
-        //         'To define the index of an array.', 'To set CSS styles for a list.'],
-        //     answer: 'To uniquely identify each element in a list.',
-        // },
-        // {
-        //     question: "Q7) What does the virtual DOM do in React",
-        //     option: ['Directly updates the real DOM.', 'Creates a copy of the real DOM and updates it in memory.',
-        //         'Is used to store state in React.', "Handles the application's routing."],
-        //     answer: 'Creates a copy of the real DOM and updates it in memory.',
-        // },
-        // {
-        //     question: "Q8) Which of the following is true about React",
-        //     option: ['React is a back-end framework.', 'React is used for building user interfaces.',
-        //         'React works only with Node.js.', "React does not support state management."],
-        //     answer: 'React is used for building user interfaces.',
-        // },
-        // {
-        //     question: "Q9) How can you pass data from a parent component to a child component",
-        //     option: ['Using state', 'Using props', 'Using useEffect', "Using context"],
-        //     answer: 'Using props',
-        // },
-        // {
-        //     question: "Q10) What is useReducer used for in React",
-        //     option: ['Managing local component state.', 'Managing complex state logic.', 'Updating the DOM.', "Handling component side effects."],
-        //     answer: 'Managing complex state logic',
-        // },
+        {
+            question: "Q3) What is JSX in React",
+            option: ['A JavaScript library for DOM manipulation.', 'A syntax extension for JavaScript that looks like XML or HTML.',
+                'A React component lifecycle method.', 'A function to update state in React.'],
+            answer: 'A syntax extension for JavaScript that looks like XML or HTML.',
+        },
+        {
+            question: "Q4) Which React hook is used to handle side effects in a functional component",
+            option: ['useState', 'useEffect', 'useRef', 'useReducer'],
+            answer: 'useEffect',
+        },
+        {
+            question: "Q5) What is the default method for creating a React component",
+            option: ['Function declaration', 'Arrow function', 'Class-based approach', 'All of the above'],
+            answer: 'All of the above',
+        },
+        {
+            question: "Q6) What is the key prop used for in a React list",
+            option: ['To identify the type of data in a list.', 'To uniquely identify each element in a list.',
+                'To define the index of an array.', 'To set CSS styles for a list.'],
+            answer: 'To uniquely identify each element in a list.',
+        },
+        {
+            question: "Q7) What does the virtual DOM do in React",
+            option: ['Directly updates the real DOM.', 'Creates a copy of the real DOM and updates it in memory.',
+                'Is used to store state in React.', "Handles the application's routing."],
+            answer: 'Creates a copy of the real DOM and updates it in memory.',
+        },
+        {
+            question: "Q8) Which of the following is true about React",
+            option: ['React is a back-end framework.', 'React is used for building user interfaces.',
+                'React works only with Node.js.', "React does not support state management."],
+            answer: 'React is used for building user interfaces.',
+        },
+        {
+            question: "Q9) How can you pass data from a parent component to a child component",
+            option: ['Using state', 'Using props', 'Using useEffect', "Using context"],
+            answer: 'Using props',
+        },
+        {
+            question: "Q10) What is useReducer used for in React",
+            option: ['Managing local component state.', 'Managing complex state logic.', 'Updating the DOM.', "Handling component side effects."],
+            answer: 'Managing complex state logic',
+        },
     ]
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [currentAnswer, setCurrentAnswer] = useState(null)
