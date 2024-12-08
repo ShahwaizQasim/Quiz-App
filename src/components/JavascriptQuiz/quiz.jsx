cf7af66d1e622069641439817436b188030c25fa
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-08 12:20:40 +0500

    code formated

--- src/components/JavascriptQuiz/quiz.jsx
+++ src/components/JavascriptQuiz/quiz.jsx
@@ -25,7 +25,8 @@ function JavaScriptQuiz() {
   return (
     <div className='h-dvh flex flex-col justify-center items-center'>
       {
-        currentQuestionIndex < quizQuestion.length ? <div className='flex flex-col justify-center'><JavascriptQuestionsList quizQuestion={quizQuestion[currentQuestionIndex].question}
+        currentQuestionIndex < quizQuestion.length ? <div className='flex flex-col justify-center'>
+          <JavascriptQuestionsList quizQuestion={quizQuestion[currentQuestionIndex].question}
           quizOptions={quizQuestion[currentQuestionIndex].options} handleClick={handleClick} currentAnswer={currentAnswer} />
           <button className={`${currentAnswer === null ? 'cursor-not-allowed mt-8' :
             'mt-10 border-2 cursor-pointer border-black py-2 px-6 text-sm rounded-[10px] hover:scale-95 font-semibold active:bg-black active:text-white text-black'} 
